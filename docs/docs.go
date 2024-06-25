ecbc7db8fbba881a10c6381126f59adee33b086f
brunoodlighthouse <brunood07@gmail.com>
2024-06-25 20:29:24 -0300

    chore: regenerating swagger

--- docs/docs.go
+++ docs/docs.go
@@ -456,7 +456,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "crud-go-4kur.onrender.com",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Swagger Example API",
